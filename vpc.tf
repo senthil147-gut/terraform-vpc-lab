7e3672728f1b8812ef3f670dcfca643d163e9f94
senthil <senthil147@gamail.com>
2026-05-11 13:36:50 +0400

    New Terfaform CI-CD project

---
+++ vpc.tf
@@ -0,0 +1,24 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "first-script-modify"
+  }
+}
+
+# Create a Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "main-public-subnet"
+  }
+}
